51756a4df64903d3cb234696328a758643c8822d
三井 陽介 <mitsuiyosuke@LAB-N1110.local>
2017-06-06 21:54:45 +0900

    mixture bernoulli

---
+++ src/mixture_bernoulli.h
@@ -0,0 +1,13 @@
+/*
+ * mixture_bernoulli.h
+ *
+ *  Created on: 2017/06/06
+ *      Author: mitsuiyosuke
+ */
+
+#ifndef SRC_MIXTURE_BERNOULLI_H_
+#define SRC_MIXTURE_BERNOULLI_H_
+
+int mixtureBernoull(unsigned int *sample_X, int n_samples, int n_dimentions, int n_components, double **means);
+
+#endif /* SRC_MIXTURE_BERNOULLI_H_ */
